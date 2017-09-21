06c56d8970949aa741014c1dd8d7397064052b2c
Krzych <krzych.pierzchala@gmail.com>
2017-09-21 07:15:16 +0200

    Fixed js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = prompt("Type triangle's base:");
-	h = prompt("Type triangle's height:");
+var a = prompt("Type triangle's base:"),
+	h = prompt("Type triangle's height:"),
 	triangleArea = 1/2 * a * h;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
